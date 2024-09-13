6db4ec19223234a2f62d9b2de7fb9717d37161bc
Ankit-0009 <ankitcode2511@gmail.com>
2024-09-13 14:18:59 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import SideBar from "./components/SideBar"
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <div className="flex">
+      <SideBar />
+      <Home />
+    </div>
+  )
+}
+
+export default App
