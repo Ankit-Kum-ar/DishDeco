e631aa05d3f8775bc5018d0446cea4abad9191e3
Ankit-0009 <ankitcode2511@gmail.com>
2024-09-14 16:35:09 +0530

    Complete the project by implementing the edamam api's and localStorage concept to retain the data after refersing the pages.

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -1,32 +1,27 @@
 import RecipeCard from "../components/RecipeCard";
 
 const Favorite = () => {
-  const fav = true;
+  const favourites = JSON.parse(localStorage.getItem("favorites")) || [];
   return (
     <div className="flex-1 p-10 min-h-screen">
       <div className="max-w-screen-lg mx-auto">
         <p className="font-bold text-3xl md:text-5xl my-4">My Favorites</p>
 
         {
-          !fav && (
+          favourites.length === 0 && (
             <div className="h-[80vh] flex flex-col md:mt-9 items-center gap-4">
               <img src="/assets/404.svg" className="h-3/4" alt="404 error" />
             </div>
           )
         }
 
-        {
-          fav && (
-            <div className="grid mt-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              <RecipeCard />
-              <RecipeCard />
-              <RecipeCard />
-              <RecipeCard />
-              <RecipeCard />
-              <RecipeCard />
-            </div>
-          )
-        }
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 md:mt-6">
+          {
+            favourites.map((recipe, index) => (
+              <RecipeCard key={index} recipe={recipe} />
+            ))
+          }
+        </div>
       </div>
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,47 @@
 import { Search } from "lucide-react"
 import RecipeCard from "../components/RecipeCard"
+import { useEffect, useState } from "react";
+import { appId, appKey } from "../utils/constant";
 
 const Home = () => {
+  const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(false);
+  
+
+  const fetchRecipes = async (searchQuery) => {
+    setLoading(true);
+    setRecipes([]);
+
+    try {
+      const response = await fetch(`https://api.edamam.com/api/recipes/v2/?app_id=${appId}&app_key=${appKey}&type=public&q=${searchQuery}`);
+      const data = await response.json();
+      console.log(data);
+      
+      setRecipes(data.hits);
+
+    } catch (error) {
+      console.error(error.message);
+    }
+    finally {
+      setLoading(false);
+    }
+    
+  }
+
+  const handleSumbit = (e) => {
+    e.preventDefault();
+    const searchQuery = e.target[0].value;
+    fetchRecipes(searchQuery);
+  }
+
+  useEffect(() => {
+    fetchRecipes("Indian");
+  }, []);
+
   return (
     <div className="p-10 flex-1 ">
       <div className="max-w-screen-lg mx-auto">
-        <form className="border-t border-t-slate-600 rounded-lg">
+        <form className="border-t border-t-slate-600 rounded-lg" onSubmit={handleSumbit}>
           <label className="input shadow-md flex items-center gap-3">
             <Search size="1.5em" />
             <input type="text" className="text-sm md:text-base grow" placeholder="What do you want to cook today ?"/>
@@ -22,13 +58,24 @@ const Home = () => {
         </p>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          <RecipeCard />
-          
+          {
+            loading ? (
+              // For 9 times to show the skeleton
+              [...Array(9)].map((_, index) => (
+                <div key={index} className="flex w-52 flex-col gap-4">
+                  <div className="skeleton h-32 w-full"></div>
+                  <div className="skeleton h-4 w-28"></div>
+                  <div className="skeleton h-4 w-full"></div>
+                  <div className="skeleton h-4 w-full"></div>
+                </div>
+              ))
+            ) : 
+            (
+              recipes.map((recipe, index) => (
+                <RecipeCard key={index} recipe={recipe.recipe} />
+              ))
+            )
+          }          
         </div>
       </div>
       
